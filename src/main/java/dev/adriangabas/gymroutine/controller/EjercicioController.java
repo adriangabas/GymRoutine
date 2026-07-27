4d2b7b4030113d0aaa0d9d51f6e074905230eb7a
Adrian Gabas Andreu <adriangabasandreu@gmail.com>
2026-07-27 13:29:09 +0200

    Añade EjercicioController

---
+++ src/main/java/dev/adriangabas/gymroutine/controller/EjercicioController.java
@@ -0,0 +1,24 @@
+package dev.adriangabas.gymroutine.controller;
+
+import dev.adriangabas.gymroutine.service.EjercicioService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ejercicios")
+public class EjercicioController {
+
+    private final EjercicioService service;
+
+    public EjercicioController(EjercicioService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String obtenerTodos(Model model) {
+        model.addAttribute("ejercicios", service.obtenerTodos());
+            return "ejercicios/lista";
+    }
+}
