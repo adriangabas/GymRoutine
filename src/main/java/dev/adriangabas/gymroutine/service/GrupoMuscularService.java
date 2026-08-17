d30c67bb4e7dd4e8be4f0e16465436f48149c764
Adrian Gabas Andreu <adriangabasandreu@gmail.com>
2026-08-17 18:10:45 +0200

    exceociones

--- src/main/java/dev/adriangabas/gymroutine/service/GrupoMuscularService.java
+++ src/main/java/dev/adriangabas/gymroutine/service/GrupoMuscularService.java
@@ -1,6 +1,8 @@
 package dev.adriangabas.gymroutine.service;
 
 import dev.adriangabas.gymroutine.entity.GrupoMuscular;
+import dev.adriangabas.gymroutine.exception.GrupoMuscularEnUsoException;
+import dev.adriangabas.gymroutine.repository.EjercicioRepository;
 import dev.adriangabas.gymroutine.repository.GrupoMuscularRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,20 +11,23 @@ import java.util.List;
 @Service
 public class GrupoMuscularService {
 
-    private final GrupoMuscularRepository repository;
     private final GrupoMuscularRepository grupoMuscularRepository;
+    private final EjercicioRepository ejercicioRepository;
 
-    public GrupoMuscularService(GrupoMuscularRepository repository, GrupoMuscularRepository grupoMuscularRepository) {
-        this.repository = repository;
+    public GrupoMuscularService(
+            GrupoMuscularRepository grupoMuscularRepository,
+            EjercicioRepository ejercicioRepository) {
+
         this.grupoMuscularRepository = grupoMuscularRepository;
+        this.ejercicioRepository = ejercicioRepository;
     }
 
     public List<GrupoMuscular> obtenerTodos() {
-        return repository.findAll();
+        return grupoMuscularRepository.findAll();
     }
 
     public GrupoMuscular obtenerPorId(Long id) {
-        return repository.findById(id)
+        return grupoMuscularRepository.findById(id)
                 .orElseThrow(() ->
                         new IllegalArgumentException(
                                 "No existe un grupo muscular con el ID: " + id
@@ -31,10 +36,18 @@ public class GrupoMuscularService {
     }
 
     public GrupoMuscular guardar(GrupoMuscular grupoMuscular) {
-        return repository.save(grupoMuscular);
+        return grupoMuscularRepository.save(grupoMuscular);
     }
 
     public void eliminar(Long id) {
-        repository.deleteById(id);
+
+        if (ejercicioRepository.existsByMusculoPrincipalId(id)) {
+
+            throw new GrupoMuscularEnUsoException(
+                    "No se puede eliminar este grupo muscular porque tiene ejercicios asociados."
+            );
+        }
+
+        grupoMuscularRepository.deleteById(id);
     }
 }
